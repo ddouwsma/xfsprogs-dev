3eb8c349c5cda8f95ef390b1b08cc05328896ad8
Chi Zhiling <chizhiling@kylinos.cn>
2025-01-16 17:09:39 +0800

    xfs_logprint: Fix super block buffer interpretation issue
    When using xfs_logprint to interpret the buffer of the super block, the
    icount will always be 6360863066640355328 (0x5846534200001000). This is
    because the offset of icount is incorrect, causing xfs_logprint to
    misinterpret the MAGIC number as icount.
    This patch fixes the offset value of the SB counters in xfs_logprint.
    
    Before this patch:
    icount: 6360863066640355328  ifree: 5242880  fdblks: 0  frext: 0
    
    After this patch:
    icount: 10240  ifree: 4906  fdblks: 37  frext: 0
    
    Suggested-by: Darrick J. Wong <djwong@kernel.org>
    Suggested-by: Dave Chinner <david@fromorbit.com>
    Signed-off-by: Chi Zhiling <chizhiling@kylinos.cn>
    Reviewed-by: Darrick J. Wong <djwong@kernel.org>

--- logprint/log_print_all.c
+++ logprint/log_print_all.c
@@ -91,22 +91,20 @@ xlog_recover_print_buffer(
 		len = item->ri_buf[i].i_len;
 		i++;
 		if (blkno == 0) { /* super block */
+			struct xfs_dsb  *dsb = (struct xfs_dsb *)p;
+
 			printf(_("	SUPER Block Buffer:\n"));
 			if (!print_buffer)
 				continue;
-		       printf(_("              icount:%llu ifree:%llu  "),
-			       (unsigned long long)
-				       be64_to_cpu(*(__be64 *)(p)),
-			       (unsigned long long)
-				       be64_to_cpu(*(__be64 *)(p+8)));
-		       printf(_("fdblks:%llu  frext:%llu\n"),
-			       (unsigned long long)
-				       be64_to_cpu(*(__be64 *)(p+16)),
-			       (unsigned long long)
-				       be64_to_cpu(*(__be64 *)(p+24)));
+			printf(_("              icount:%llu ifree:%llu  "),
+				(unsigned long long) get_unaligned_be64(&dsb->sb_icount),
+				(unsigned long long) get_unaligned_be64(&dsb->sb_ifree));
+			printf(_("fdblks:%llu  frext:%llu\n"),
+				(unsigned long long) get_unaligned_be64(&dsb->sb_fdblocks),
+				(unsigned long long) get_unaligned_be64(&dsb->sb_frextents));
 			printf(_("		sunit:%u  swidth:%u\n"),
-			       be32_to_cpu(*(__be32 *)(p+56)),
-			       be32_to_cpu(*(__be32 *)(p+60)));
+				get_unaligned_be32(&dsb->sb_unit),
+				get_unaligned_be32(&dsb->sb_width));
 		} else if (be32_to_cpu(*(__be32 *)p) == XFS_AGI_MAGIC) {
 			int bucket, buckets;
 			agi = (xfs_agi_t *)p;
